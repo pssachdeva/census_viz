3d6ebdf4142c2fe0f6e481e25737fdfe8856bd4e
Jesse Livezey <jesse.livezey@gmail.com>
2018-04-22 11:50:32 -0700

    all hierarchy

--- utils.py
+++ utils.py
@@ -2,43 +2,60 @@ import numpy as np
 import pandas as pd
 
 def parse_index(index):
-    collapse = index[0]
-    highest = index[1:3]
-    measure = index.split('_')[0][3:6]
+    TableType = index[0]
+    Subject = index[1:3]
+    TableNumber = index.split('_')[0][3:6]
     letters = index.split('_')[0][6:]
-    label = index.split('_')[1][:-1]
-    ME = index[-1]
-    return collapse, highest, measure, letters, label, ME
+    if 'PR' in [letters]:
+        PR = 'PR'
+        RaceIteration = letters[:-2]
+    else:
+        PR = ''
+        RaceIteration = letters
+    SubCategory = index.split('_')[1][:-1]
+    EM = index[-1]
+    return TableType, Subject, TableNumber, RaceIteration, PR, SubCategory, EM
 
 def make_hierarchical(df):
     keep_cols = df.columns.values.tolist()
     df = df.loc[df.index.str.contains('_')]
-    df.loc[:, 'init_letter'] = np.nan
-    df.loc[:, 'highest'] = np.nan
-    df.loc[:, 'measure'] = np.nan
-    df.loc[:, 'letters'] = np.nan
-    df.loc[:, 'label'] = np.nan
-    df.loc[:, 'ME'] = np.nan
+    df.loc[:, 'TableType'] = np.nan
+    df.loc[:, 'Subject'] = np.nan
+    df.loc[:, 'TableNumber'] = np.nan
+    df.loc[:, 'RaceIteration'] = np.nan
+    df.loc[:, 'PR'] = np.nan
+    df.loc[:, 'SubCategory'] = np.nan
+    df.loc[:, 'EM'] = np.nan
 
-    init_letters = []
-    highests = []
-    measures = []
-    letters = []
-    labels = []
-    MEs = []
+    TableTypes = []
+    Subjects = []
+    TableNumbers = []
+    RaceIterations = []
+    PRs = []
+    SubCategorys = []
+    EMs = []
 
     for v in df.iterrows():
         idx = v[0]
-        init_letter, highest, measure, letter, label, ME = parse_variable(idx)
-        init_letters.append(init_letter)
-        highests.append(highest)
-        measures.append(measure)
-        letters.append(letter)
-        labels.append(label)
-        MEs.append(ME)
-    df_M = pd.DataFrame(df.values, index=[init_letters, highests, measures, letters, labels, MEs])
-    df_M.columns = df.columns
-    return df_M[keep_cols]
+        TableType, Subject, TableNumber, RaceIteration, PR, SubCategory, EM = parse_index(idx)
+        TableTypes.append(TableType)
+        Subjects.append(Subject)
+        TableNumbers.append(TableNumber)
+        RaceIterations.append(RaceIteration)
+        PRs.append(PR)
+        SubCategorys.append(SubCategory)
+        EMs.append(EM)
+    df['TableType'] = TableTypes
+    df['Subject'] = Subjects
+    df['TableNumber'] = TableNumbers
+    df['RaceIteration'] = RaceIterations
+    df['PR'] = PRs
+    df['SubCategory'] = SubCategorys
+    df['EM'] = EMs
+    df = df.set_index(['TableType', 'Subject', 'TableNumber', 'RaceIteration', 'PR', 'SubCategory', 'EM'])
+    #df_M = pd.DataFrame(df.values, index=[init_letters, highests, measures, letters, labels, MEs])
+    #df_M.columns = df.columns
+    return df[keep_cols]
 
 def combine_index(collapse, highest, measure, letters, label, ME):
     return collaplse + highest + measure + letters + '_' + label + M
